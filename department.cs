ffb47d7c9ff8e6ca054c76e34b3e42110fde975d
oliver-tbs <oliver.postlethwaite@tbs-sct.gc.ca>
2024-01-31 11:45:58 -0500

    Updated response to only include required fields

--- department.cs
+++ department.cs
@@ -9,11 +9,35 @@ using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Graph.Models;
 using Microsoft.Kiota.Abstractions;
+using ListItem = Microsoft.Graph.Models.ListItem;
+using Newtonsoft.Json;
 
 namespace appsvc_fnc_dev_OrgInfoList
 {
     public static class department
     {
+        private class Department
+        {
+            public string Legal_x0020_Title { get; set; }
+            public string Appellation_x0020_l_x00e9_gale { get; set; }
+            public string Abbr_x002e_ { get; set; }
+            public string Abr_x00e9_v_x002e_ { get; set; }
+            public string RG_x0020_Code { get; set; }
+            public string B2B { get; set; }
+            public string ProBList { get; set; }
+
+            public Department(string TitleEn, string TitleFr, string abbreviationEn, string abbreviationFr, string RGCode, string _B2B, string _ProBList)
+            {
+                Legal_x0020_Title = TitleEn;
+                Appellation_x0020_l_x00e9_gale = TitleFr;
+                Abbr_x002e_ = abbreviationEn;
+                Abr_x00e9_v_x002e_ = abbreviationFr;
+                RG_x0020_Code = RGCode;
+                B2B = _B2B;
+                ProBList = _ProBList;
+            }
+        }
+
         [FunctionName("Department")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.System, "get", "post", Route = null)] HttpRequest req,
@@ -35,11 +59,13 @@ namespace appsvc_fnc_dev_OrgInfoList
             try
             {
                 List<ListItem> itemList = new List<ListItem>();
+                List<Department> departments = new();
 
                 var items = await graphAPIAuth.Sites[siteid].Lists[listid].Items.GetAsync((requestConfiguration) =>
                 {
                     requestConfiguration.QueryParameters.Expand = new string[] { "fields($select=Legal_x0020_Title,Appellation_x0020_l_x00e9_gale,Abbr_x002e_,Abr_x00e9_v_x002e_,RG_x0020_Code,B2B,ProBList)" };
                 });
+
                 itemList.AddRange(items.Value);
 
                 while (items.OdataNextLink != null)
@@ -54,7 +80,12 @@ namespace appsvc_fnc_dev_OrgInfoList
                     itemList.AddRange(items.Value);
                 }
 
-                return new OkObjectResult(itemList);
+                foreach (var item in itemList)
+                {
+                    departments.Add(new Department(GetValue(item, "Legal_x0020_Title"), GetValue(item, "Appellation_x0020_l_x00e9_gale"), GetValue(item, "Abbr_x002e_"), GetValue(item, "Abr_x00e9_v_x002e_"), GetValue(item, "RG_x0020_Code"), GetValue(item, "B2B"), GetValue(item, "ProBList")));
+                }
+
+                return new OkObjectResult(JsonConvert.SerializeObject(departments));
             }
             catch (Exception ex)
             {
@@ -62,6 +93,11 @@ namespace appsvc_fnc_dev_OrgInfoList
                 return new BadRequestObjectResult(ex);
 
             }
+        }
+
+        private static string GetValue(ListItem item, string fieldName)
+        {
+            return item.Fields.AdditionalData.ContainsKey(fieldName) ? item.Fields.AdditionalData[fieldName].ToString() : "";
         }
     }
 }
--- domain.cs
+++ domain.cs
@@ -9,11 +9,32 @@ using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Graph.Models;
 using Microsoft.Kiota.Abstractions;
+using Newtonsoft.Json;
 
 namespace appsvc_fnc_dev_OrgInfoList
 {
     public static class domain
     {
+        private class Domain
+        {
+            public string Legal_x0020_Title { get; set; }
+            public string Appellation_x0020_l_x00e9_gale { get; set; }
+            public string Abbr_x002e_ { get; set; }
+            public string Abr_x00e9_v_x002e_ { get; set; }
+            public string RG_x0020_Code { get; set; }
+            public string GoCDomain { get; set; }
+
+            public Domain(string TitleEn, string TitleFr, string abbreviationEn, string abbreviationFr, string RGCode, string _GoCDomain)
+            {
+                Legal_x0020_Title = TitleEn;
+                Appellation_x0020_l_x00e9_gale = TitleFr;
+                Abbr_x002e_ = abbreviationEn;
+                Abr_x00e9_v_x002e_ = abbreviationFr;
+                RG_x0020_Code = RGCode;
+                GoCDomain = _GoCDomain;
+            }
+        }
+
         [FunctionName("Domain")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.System, "get", "post", Route = null)] HttpRequest req,
@@ -35,11 +56,13 @@ namespace appsvc_fnc_dev_OrgInfoList
             try
             {
                 List<ListItem> itemList = new List<ListItem>();
+                List<Domain> domains = new();
 
                 var items = await graphAPIAuth.Sites[siteid].Lists[listid].Items.GetAsync((requestConfiguration) =>
                 {
                     requestConfiguration.QueryParameters.Expand = new string[] { "fields($select=Legal_x0020_Title,Appellation_x0020_l_x00e9_gale,Abbr_x002e_,Abr_x00e9_v_x002e_,RG_x0020_Code,GoCDomain)" };
                 });
+
                 itemList.AddRange(items.Value);
 
                 while (items.OdataNextLink != null)
@@ -54,13 +77,23 @@ namespace appsvc_fnc_dev_OrgInfoList
                     itemList.AddRange(items.Value);
                 }
 
-                return new OkObjectResult(itemList);
+                foreach (var item in itemList)
+                {
+                    domains.Add(new Domain(GetValue(item, "Legal_x0020_Title"), GetValue(item, "Appellation_x0020_l_x00e9_gale"), GetValue(item, "Abbr_x002e_"), GetValue(item, "Abr_x00e9_v_x002e_"), GetValue(item, "RG_x0020_Code"), GetValue(item, "GoCDomain")));
+                }
+
+                return new OkObjectResult(JsonConvert.SerializeObject(domains));
             }
             catch (Exception ex)
             {
                 log.LogInformation($"Error when try get all domain {ex}");
                 return new BadRequestObjectResult(ex);
             }
+        }
+
+        private static string GetValue(ListItem item, string fieldName)
+        {
+            return item.Fields.AdditionalData.ContainsKey(fieldName) ? item.Fields.AdditionalData[fieldName].ToString() : "";
         }
     }
 }
